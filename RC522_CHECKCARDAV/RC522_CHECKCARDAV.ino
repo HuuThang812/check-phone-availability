6f10bb1c4ec587759bd35e6ef9b5484873535fe9
Thắng Hữu <thanghuu911@Gmail.com>
2025-10-29 22:29:11 +0700

    udpate1

---
+++ RC522_CHECKCARDAV/RC522_CHECKCARDAV.ino
@@ -0,0 +1,58 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 21   // SDA
+#define RST_PIN 22  // RST
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+
+void setup()
+{
+  Serial.begin(115200);
+  SPI.begin(18,19,23,21);  // SPI.begin(SCK, MISO, MOSI, SS_optional)
+  mfrc522.PCD_Init();// Khởi tạo RC522
+  Serial.println("da san sang doc the");
+}
+
+
+
+bool hasCard() {
+  byte atqa[2];
+  byte atqaLen = 2;
+  // Gửi lệnh REQA và xem phản hồi
+  MFRC522::StatusCode status = mfrc522.PICC_RequestA(atqa, &atqaLen);
+  return (status == MFRC522::STATUS_OK);  // true nếu có thẻ
+}
+
+
+
+void loop() {
+  if (hasCard()) {
+    Serial.println("Co the trong vung doc");
+    delay(1000);
+  } else {
+    delay (500);
+    if (!hasCard()){
+
+    
+    Serial.println("Khong co the");
+    delay(1000);
+    return;
+    }
+  }
+
+  delay(500);
+mfrc522.PICC_ReadCardSerial();
+
+ Serial.print("UID Thẻ: ");
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+    Serial.print(" ");
+  }
+  
+  delay (1000);
+
+
+
+
+
+}
